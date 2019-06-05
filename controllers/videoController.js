57e3b631b6903463da465aaf7edc5d14335d3952
jupang21 <46041397+jupang21@users.noreply.github.com>
2019-06-05 15:30:12 +0900

    #2.12
    Controller finish

---
+++ controllers/videoController.js
@@ -0,0 +1,7 @@
+export const home = (req, res) => res.send("Home");
+export const search = (req, res) => res.send("Search");
+export const videos = (req, res) => res.send("Vidoes");
+export const upload = (req, res) => res.send("Upload");
+export const videoDetail = (req, res) => res.send("Vidoe Detail");
+export const editVideo = (req, res) => res.send("Edit Vidoe");
+export const deleteVideo = (req, res) => res.send("Delete Vidoe");
