8c71af11cf86af965c2e03bf7f76da741bac6a6f
jupang21 <46041397+jupang21@users.noreply.github.com>
2019-06-07 00:35:34 +0900

    Revert "Revert "#2.14 layouts""
    This reverts commit 727e8531df93c91c8a7271b942ec2914249bb015.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("Vidoes");
-export const upload = (req, res) => res.send("Upload");
-export const videoDetail = (req, res) => res.send("Vidoe Detail");
-export const editVideo = (req, res) => res.send("Edit Vidoe");
-export const deleteVideo = (req, res) => res.send("Delete Vidoe");
+export const home = (req, res) => res.render("home");
+export const search = (req, res) => res.render("search");
+export const videos = (req, res) => res.render("vidoes");
+export const upload = (req, res) => res.render("upload");
+export const videoDetail = (req, res) => res.render("vidoeDetail");
+export const editVideo = (req, res) => res.render("editVidoe");
+export const deleteVideo = (req, res) => res.render("deleteVidoe");
