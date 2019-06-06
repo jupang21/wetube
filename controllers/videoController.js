c080d26a944ee5476f0b241477cd576b69532b9e
jupang21 <46041397+jupang21@users.noreply.github.com>
2019-06-07 01:36:46 +0900

    #2.17
    Template Variables
    
    render 함수의
    첫번째 인자는 템플릿, 두번째 인자는 템플릿에 추가할 정보가 담긴 객체

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,13 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
-export const videos = (req, res) => res.render("vidoes");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("vidoeDetail");
-export const editVideo = (req, res) => res.render("editVidoe");
-export const deleteVideo = (req, res) => res.render("deleteVidoe");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) =>
+  res.render("search", { pageTitle: "Search" });
+export const videos = (req, res) =>
+  res.render("vidoes", { pageTitle: "Videos" });
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
